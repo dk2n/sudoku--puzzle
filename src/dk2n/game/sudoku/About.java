894702bbf45e361be51fc0019008cfa2cb9cb3c1
Derek Nguyen <Derek Nguyen>
2014-04-07 15:54:50 -0700

    Finished

---
+++ src/dk2n/game/sudoku/About.java
@@ -0,0 +1,15 @@
+package dk2n.game.sudoku;
+
+import com.infrolog.elu.android.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class About extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about);
+	}
+}
